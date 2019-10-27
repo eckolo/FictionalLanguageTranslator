d245ae408857f4675b39835ae4b44244d00d9620
eckolo <eckolo.0722@gmail.com>
2019-10-27 14:29:58 +0900

    ざっとIF周りをでっちあげる

---
+++ Controllers/GermanStyleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FictionalLanguageTranslator.Models.Application.Value;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FictionalLanguageTranslator.Controllers
+{
+    public class GermanStyleController : Controller
+    {
+        public IActionResult Index(string japanese = null)
+        {
+            var japaneseText = japanese ?? "";
+
+            var model = new GermanStyleModel(japaneseText, "");
+
+            return View(model);
+        }
+    }
+}
---
+++ Models/Application/Value/GermanStyleModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FictionalLanguageTranslator.Models.Application.Value
+{
+    public class GermanStyleModel
+    {
+        public GermanStyleModel(string japanese, string fictional)
+        {
+            this.japanese = japanese ?? throw new ArgumentNullException(nameof(japanese));
+            this.fictional = fictional ?? throw new ArgumentNullException(nameof(fictional));
+        }
+
+        public string japanese { get; }
+        public string fictional { get; }
+    }
+}
