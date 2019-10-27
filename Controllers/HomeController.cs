34a63134e57dc0f3aecc1a00e5a961f3772160e7
eckolo <eckolo.0722@gmail.com>
2019-10-27 14:45:33 +0900

    ErrorViewModelの場所を移動

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using FictionalLanguageTranslator.Models;
+using FictionalLanguageTranslator.Models.Application.Value;
 
 namespace FictionalLanguageTranslator.Controllers
 {
