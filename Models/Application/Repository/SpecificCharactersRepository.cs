6ae57506bee19bef589b4b9d9d4d39d07215ef11
eckolo <eckolo.0722@gmail.com>
2019-10-27 15:15:20 +0900

    特定文字列類のデータ定義

---
+++ Models/Application/Repository/SpecificCharactersRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FictionalLanguageTranslator.Models.Application.Repository
+{
+    public static class SpecificCharactersRepository
+    {
+        /// <summary>
+        /// 子音
+        /// </summary>
+        public static readonly IEnumerable<string> consonants = new[]
+        {
+            "",
+            "b",
+            "c", "ch", "chs",
+            "d", "dt",
+            "f",
+            "g",
+            "h",
+            "ig",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n", "ng",
+            "p", "pf",
+            "qu",
+            "r", "rh",
+            "s", "sch", "sp", "ss", "st", "B",
+            "t", "th", "ti", "ts", "tz", "tsch",
+            "v",
+            "w",
+            "x",
+            "z",
+        };
+        /// <summary>
+        /// 母音
+        /// </summary>
+        public static readonly IEnumerable<string> vowels = new[]
+        {
+            "a", "aa", "au",
+            "e", "ee", "ei", "er", "eu",
+            "i", "ie",
+            "o", "oo",
+            "u",
+            "a:", "a:u",
+            "o:",
+            "u:", "u:r",
+        };
+        /// <summary>
+        /// 記号
+        /// </summary>
+        public static readonly IEnumerable<string> specialChars = new[]
+        {
+            "\r", "\n", "\r\n",
+            "!", "?", "！", "？",
+            "「", "」",
+            "…",
+            ",", ".", "、", "。",
+            " ", "　",
+        };
+    }
+}
