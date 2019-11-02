54e8ff9489c66db90de8f81f02ce93b9dd163e60
eckolo <eckolo.0722@gmail.com>
2019-11-02 19:01:11 +0900

    文字数調整

--- Models/Application/Service/CodePointConverter.cs
+++ Models/Application/Service/CodePointConverter.cs
@@ -18,6 +18,7 @@ namespace FictionalLanguageTranslator.Models.Application.Service
         public static ulong ToCodePoint(this string text)
             => text
             .Select(code => (ulong)code)
+            .Select(code => ((code - (code % 0x100)) / 0x100) + (code % 0x100))
             .Aggregate((code1, code2) => code1 * 0x10 + code2);
     }
 }
